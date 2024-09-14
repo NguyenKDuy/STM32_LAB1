83ea4f6681d323f4959272c4cc54d3aa2860ff5e
DUY NGUYEN KHAC <nguyenkhacduy16112004@gmail.com>
2024-09-14 15:17:02 +0700

    Ex7 Done

--- LAB 1/Core/Src/main.c
+++ LAB 1/Core/Src/main.c
@@ -32,6 +32,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+void clearAllClock();
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -54,49 +55,22 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-void test12LED(int num){
-	switch (num) {
-	case 1:
-				HAL_GPIO_TogglePin(LED_1_GPIO_Port, LED_1_Pin);
-				break;
-	case 2:
-				HAL_GPIO_TogglePin(LED_2_GPIO_Port, LED_2_Pin);
-				break;
-	case 3:
-				HAL_GPIO_TogglePin(LED_3_GPIO_Port, LED_3_Pin);
-				break;
-	case 4:
-				HAL_GPIO_TogglePin(LED_4_GPIO_Port, LED_4_Pin);
-				break;
-	case 5:
-				HAL_GPIO_TogglePin(LED_5_GPIO_Port, LED_5_Pin);
-				break;
-	case 6:
-				HAL_GPIO_TogglePin(LED_6_GPIO_Port, LED_6_Pin);
-				break;
-	case 7:
-				HAL_GPIO_TogglePin(LED_7_GPIO_Port, LED_7_Pin);
-				break;
-	case 8:
-				HAL_GPIO_TogglePin(LED_8_GPIO_Port, LED_8_Pin);
-				break;
-	case 9:
-				HAL_GPIO_TogglePin(LED_9_GPIO_Port, LED_9_Pin);
-				break;
-	case 10:
-				HAL_GPIO_TogglePin(LED_10_GPIO_Port, LED_10_Pin);
-				break;
-	case 11:
-				HAL_GPIO_TogglePin(LED_11_GPIO_Port, LED_11_Pin);
-				break;
-	case 12:
-				HAL_GPIO_TogglePin(LED_12_GPIO_Port, LED_12_Pin);
-				break;
-	default:
-				break;
-	}
-}
 
+
+void clearAllClock(){
+	HAL_GPIO_WritePin (LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin (LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin (LED_3_GPIO_Port, LED_3_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin (LED_4_GPIO_Port, LED_4_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin (LED_5_GPIO_Port, LED_5_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin (LED_6_GPIO_Port, LED_6_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin (LED_7_GPIO_Port, LED_7_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin (LED_8_GPIO_Port, LED_8_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin (LED_9_GPIO_Port, LED_9_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin (LED_10_GPIO_Port, LED_10_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin (LED_11_GPIO_Port, LED_11_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin (LED_12_GPIO_Port, LED_12_Pin, GPIO_PIN_SET);
+}
 /* USER CODE END 0 */
 
 /**
@@ -106,7 +80,6 @@ void test12LED(int num){
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	int counter = 1;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -128,7 +101,6 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -136,10 +108,9 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	  if (counter > 12) counter = 1;
-	  test12LED(counter++);
 	  HAL_Delay(1000);
-
+	  clearAllClock();
+	  break;
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
